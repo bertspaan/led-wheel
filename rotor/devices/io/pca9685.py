afcc4f965b6cd9386f18d28cb7bd3c1d341ae3da
Bert Spaan <bert.spaan@gmail.com>
2018-03-28 17:39:20 +0200

    added led mapping

--- rotor/devices/io/pca9685.py
+++ rotor/devices/io/pca9685.py
@@ -1,22 +1,88 @@
 import time
 from threading import Thread
 import Adafruit_PCA9685
+import math
 
 class PCA9685:
 
+    frequency = 1000
+
+    ceiling = [1, 14]
+
+    mapping = [
+        [0, 0], # First of first PCA9685
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 6],
+        [0, 4],
+        [0, 5],
+        [0, 7],
+        [0, 8],
+        [0, 9],
+        [0, 10],
+        [0, 11],
+        [0, 12],
+        [0, 13],
+        [0, 14],
+        [1, 15], # First of second PCA9685
+        [1, 13],
+        [1, 12],
+        [1, 11],
+        [1, 10],
+        [1, 9],
+        [1, 8],
+        [1, 7],
+        [1, 6],
+        [1, 5],
+        [1, 4],
+        [1, 3],
+        [1, 2],
+        [1, 1],
+        [1, 0]
+    ]
+
     def __init__(self):
-        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwms = [
+            Adafruit_PCA9685.PCA9685(0x40),
+            Adafruit_PCA9685.PCA9685(0x41)
+        ]
+
+    def set_led(self, index, value):
+        led = self.mapping[index]
+
+        max = 4095
+        # value between 0 and 1
+        on = 0
+        off = round(max * value)
+
+        pwm = self.pwms[led[0]]
+        pwm.set_pwm(led[1], on, off)
 
     def run(self):
-        self.pwm.set_pwm_freq(60)
+        for pwm in self.pwms:
+            pwm.set_pwm_freq(self.frequency)
+
+        led = 0
 
         while True:
-            pwm.set_pwm(0, 0, 0)
-            time.sleep(1)
-            pwm.set_pwm(0, 4096, 4096)
-            time.sleep(1)
+
+            for index in range(0, len(self.mapping)):
+                value = 0
+
+                if abs(led - index) <= 5:
+                    value = (math.sin(abs(led - index) / 2) + 1) / 2
+
+                value = 0
+                self.set_led(index, value)
+
+
+            led = (led + 1) % len(self.mapping)
+            time.sleep(0.02)
+
+
 
     def start(self):
-        print("Starting GPIO!")
+        print("Starting PCA9685!")
         thread = Thread(target=self.run)
         thread.start()
